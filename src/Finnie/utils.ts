97568f0d67cb64e1bff1cae03476e9575bba10ef
EllieAzaveda <elliemiller66@icloud.com>
2021-10-08 14:00:23 -0600

    Add utils and build nsfw
    Add utils file and move helper methods; Build NSFW functionality and helper method
    
    Co-authored-by: Jackson McGuire <jaxmcguire@gmail.com>

---
+++ src/Finnie/utils.ts
@@ -0,0 +1,55 @@
+declare global {
+  interface Window {
+    koiiWallet?: WindowFinnie;
+  }
+}
+// Waiting for these
+export const markNsfwUrl = "";
+export const voteNsfwUrl = "";
+export const adminAddress = "secrets.adminAddress";
+
+export interface ResponseObject {
+  data: any[];
+  status: string;
+}
+export interface WindowFinnie {
+  getPermissions?(): Promise<any> | ResponseObject;
+  connect?(): Promise<any>;
+  sendKoii?(address: string, amount: number): Promise<any>;
+  getAddress?(): Promise<any>;
+  disconnect?(): Promise<any>;
+  registerData?(): Promise<any>;
+  sign?(): Promise<any>;
+  signPort?({ data: string }): any;
+}
+
+export const checkForFinnie = (): Promise<any> => {
+  const extensionCall = () => window.koiiWallet;
+  let counter = 0;
+  const checkCondition = (resolve, reject) => {
+    const result = extensionCall();
+    if (result) {
+      resolve(result);
+    } else if (counter < 5) {
+      counter++;
+      setTimeout(checkCondition, 200, resolve, reject);
+    } else {
+      resolve(false);
+    }
+  };
+  return new Promise(checkCondition);
+};
+
+// helper function for voteNSFW
+export const fetchNSFW = async (url, id, signature?): Promise<any> => {
+  // Body with signature does not have stringify in example docs
+  const body = signature ? JSON.stringify({ signature, NFTid: id }) : JSON.stringify({ NFTId: id });
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: body,
+  };
+  return await fetch(url, postOptions);
+};
